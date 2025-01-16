1ba5676feffddf58db50aa983cc944db6c6d3227
Paweł Kruszko <pawelkruszko@gmail.com>
2025-01-16 20:30:05 +0100

    update default email

--- django_project/settings.py
+++ django_project/settings.py
@@ -151,6 +151,7 @@ LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
 
 SITE_ID = 1
+
 AUTHENTICATION_BACKENDS = (
     "django.contrib.auth.backends.ModelBackend",
     "allauth.account.auth_backends.AuthenticationBackend",
