67bdc93a9c2359410278e0968f646c1fdf9e3f17
Paweł Kruszko <pawelkruszko@gmail.com>
2025-01-24 19:02:14 +0100

    uuid & tests added

--- books/views.py
+++ books/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Books
 
@@ -7,3 +7,9 @@ class BookListView(ListView):
     model = Books
     context_object_name = "book_list"
     template_name = "books/book_list.html"
+
+
+class BookDetailView(DetailView):
+    model = Books
+    context_object_name = "book"
+    template_name = "books/book_detail.html"
